b00904a5785a50de635f7bcf02e0e925506c83c1
unknown <thais.s.land@gmail.com>
2023-04-07 19:53:49 -0300

    criando projeto e database

---
+++ tatitata/src/test/java/com/tatitata/tatitata/TatitataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tatitata.tatitata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TatitataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
